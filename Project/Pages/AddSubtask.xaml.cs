95d2aa4f847a75f446e676c8a707dbe5e38a2076
Daria <kobelevadara210@gmail.com>
2025-05-17 00:01:05 +0500

    Разработала интерфейс для подзадач (подробнее и добавление). Кобелева.

--- Project/Pages/AddSubtask.xaml.cs
+++ Project/Pages/AddSubtask.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Project.Pages;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,14 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Project
+namespace Project.Pages
 {
-    public partial class MainWindow : Window
+    /// <summary>
+    /// Логика взаимодействия для AddSubtask.xaml
+    /// </summary>
+    public partial class AddSubtask : Page
     {
-        public MainWindow()
+        public AddSubtask()
         {
             InitializeComponent();
-            MainFrame.Navigate(new Login());
         }
     }
 }
--- Project/Pages/SubtaskDetails.xaml.cs
+++ Project/Pages/SubtaskDetails.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Project.Pages;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,14 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Project
+namespace Project.Pages
 {
-    public partial class MainWindow : Window
+    /// <summary>
+    /// Логика взаимодействия для SubtaskDetails.xaml
+    /// </summary>
+    public partial class SubtaskDetails : Page
     {
-        public MainWindow()
+        public SubtaskDetails()
         {
             InitializeComponent();
-            MainFrame.Navigate(new Login());
         }
     }
 }
