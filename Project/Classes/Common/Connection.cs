c0e7539a52b3d01eb9e246c8b3757e48695c7b71
Daria <kobelevadara210@gmail.com>
2025-05-21 08:47:57 +0500

    Начало работы 21.05

--- Project/Classes/Common/Connection.cs
+++ Project/Classes/Common/Connection.cs
@@ -5,23 +5,14 @@ namespace Project.Classes.Common
 {
     public class Connection
     {
-        // Ensure the connection string is correct
-        public static readonly string config = "server=127.0.0.1;port=3307;uid=root;pwd=;database=project;";
+        public static readonly string config = "server=127.0.0.1;uid=root;pwd=;database=project;";
 
         public static MySqlConnection OpenConnection()
         {
             MySqlConnection connection = new MySqlConnection(config);
-            try
-            {
-                connection.Open();
-                return connection;
-            }
-            catch (MySqlException ex)
-            {
-                // Log the exception details for debugging
-                Console.WriteLine($"Error connecting to MySQL: {ex.Message}");
-                throw; // Re-throw the exception to handle it in the calling method
-            }
+            connection.Open();
+
+            return connection;
         }
 
         public static MySqlDataReader Query(string SQL, MySqlConnection connection)
@@ -31,11 +22,10 @@ namespace Project.Classes.Common
 
         public static void CloseConnection(MySqlConnection connection)
         {
-            if (connection != null)
-            {
-                connection.Close();
-                MySqlConnection.ClearPool(connection);
-            }
+            
+            connection.Close();
+            MySqlConnection.ClearPool(connection);
+          
         }
     }
 }
