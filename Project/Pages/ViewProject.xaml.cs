df81223972a9887a2549bc20a43c2214c1b04fae
Anna <eltyshevaanna2006@yandex.ru>
2025-05-21 12:56:05 +0500

    функционал просмотра проекта

--- Project/Pages/ViewProject.xaml.cs
+++ Project/Pages/ViewProject.xaml.cs
@@ -1,33 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Project.Classes;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Project.Pages
 {
-    /// <summary>
-    /// Логика взаимодействия для ViewProject.xaml
-    /// </summary>
     public partial class ViewProject : Page
     {
-        public ViewProject()
+        private int _projectId;
+        private Classes.Project _currentProject;
+
+        public ViewProject(int projectId)
         {
             InitializeComponent();
+            _projectId = projectId;
+            LoadProjectData();
+        }
+
+        private void LoadProjectData()
+        {
+            _currentProject = ProjectContext.Get().FirstOrDefault(p => p.Id == _projectId);
+            if (_currentProject == null)
+            {
+                MessageBox.Show("Проект не найден!");
+                NavigationService?.GoBack();
+                return;
+            }
+
+         
+            NameProjectLabel.Content = _currentProject.Name;
+            DescriptionLabel.Content = _currentProject.Description ?? "Описание отсутствует";
+            PublicityLabel.Content = _currentProject.IsPublic ? "Публичный" : "Закрытый";
+
+  
+            var creatorRelation = ProjUserContext.GetByProjectId(_projectId)
+                .FirstOrDefault(pu => pu.Role == "Создатель");
+
+            if (creatorRelation != null)
+            {
+                var creator = UserContext.Get().FirstOrDefault(u => u.Id == creatorRelation.User);
+                CreatorLabel.Content = creator != null ?
+                    $"Создатель: {creator.FullName}" :
+                    "Создатель: Неизвестен";
+            }
+
+            LoadProjectMembers();
+        }
+
+        private void LoadProjectMembers()
+        {
+            MembersListView.Items.Clear();
+
+            
+            var projectMembers = ProjUserContext.GetByProjectId(_projectId);
+            var allUsers = UserContext.Get();
+
+            foreach (var member in projectMembers)
+            {
+                var user = allUsers.FirstOrDefault(u => u.Id == member.User);
+                if (user != null)
+                {
+                   
+                    var memberInfo = $"{user.FullName} ({member.Role})";
+                    MembersListView.Items.Add(new ListViewItem { Content = memberInfo });
+                }
+            }
+
+            
+            if (MembersListView.Items.Count == 0)
+            {
+                MembersListView.Items.Add(new ListViewItem
+                {
+                    Content = "В проекте нет участников",
+                    Foreground = System.Windows.Media.Brushes.Gray
+                });
+            }
         }
 
         private void Bt8_Close(object sender, RoutedEventArgs e)
         {
-
+            
+            NavigationService?.GoBack();
         }
     }
 }
